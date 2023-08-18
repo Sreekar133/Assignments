9c2824d6a12d0395c78eb1d98e5c5e659e17289e
Sreekar133 <95556108+Sreekar133@users.noreply.github.com>
2023-08-18 06:53:56 +0530

    Update sql assignment.sql

--- sql assignment.sql
+++ sql assignment.sql
@@ -1,7 +1,7 @@
 create table store_sales(month varchar(5) not null,prod_name varchar(15) not null,sales int not null)
 
 insert into store_sales values('Jan','Fruits',45000),
-							  ('Jan','Vegetables',67000),
+			      ('Jan','Vegetables',67000),
                               ('Jan','Dairy',55000),
                               ('Feb','Fruits',42000),
                               ('Feb','Vegetables',32000),
